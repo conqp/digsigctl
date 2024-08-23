28874a60588618f2511df9ed7c97724a88e1b59f
Richard Neumann <mail@richard-neumann.de>
2024-08-23 18:36:02 +0200

    Skip erroneous disks

--- src/sysinfo/smart.rs
+++ src/sysinfo/smart.rs
@@ -1,11 +1,15 @@
 use libatasmart::Disk;
+use rocket::log::private::error;
 use sysinfo::Disks;
 
 pub fn smart_status_ok() -> bool {
-    Disks::new_with_refreshed_list().list().iter().all(|disk| {
-        Disk::new(disk.name().as_ref())
-            .expect("failed to create disk")
-            .get_smart_status()
-            .unwrap_or(false)
-    })
+    Disks::new_with_refreshed_list()
+        .list()
+        .iter()
+        .filter_map(|disk| {
+            Disk::new(disk.name().as_ref())
+                .inspect_err(|error| error!("{error}"))
+                .ok()
+        })
+        .all(|mut disk| disk.get_smart_status().unwrap_or(false))
 }
